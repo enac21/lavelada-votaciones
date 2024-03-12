05201281ef8e22ceeaa69ddebba64ae239a25900
enac21 <96846797+enac21@users.noreply.github.com>
2024-03-12 17:43:53 +0000

    vote system localstorage and voted boxers section after voting

--- src/components/VoteSystem.tsx
+++ src/components/VoteSystem.tsx
@@ -1,66 +1,83 @@
-import { useState } from "preact/hooks"
+import { useState, useEffect } from "preact/hooks"
 
 import CombatsInfo from "@/data/combates.json"
 
-interface CombatInfo {
-    id: number,
-    title: string,
-    combatTitle: string,
-    boxers: Boxer[]
-}
-
-interface Boxer {
-    id: string,
-    boxerName: string,
-    image: string,
-    height: string,
-    weight: string,
-}
-
-type Votes = Array<Array<string>>
+type Votes = Array<string>
 
 export function VoteSystem () {
-    const [combatInfo] = useState<CombatInfo[]>(CombatsInfo)
-    const [votes, setVotes] = useState<Votes>(Array.from({ length: combatInfo.length }, () => []))
-    const allVoted = votes.every(combatVote => combatVote.length > 0);
+    const combatInfo = CombatsInfo
+    
+    const [votes, setVotes] = useState<Votes>(Array.from({ length: combatInfo.length }))
+    const allVoted = votes.every(combatVote => combatVote != undefined);
+    
+    const [alreadyVoted, setAlreadyVoted] = useState(false)
+    const [storageVotes, setStorageVotes] = useState<Votes>()
+    
+    useEffect(() => {
+        const storageVotesJSON = localStorage.getItem('votes');
+        if (storageVotesJSON) {
+            const storageVotes = JSON.parse(storageVotesJSON);
+            setStorageVotes(storageVotes);
+            setAlreadyVoted(true)
+        }
+    }, [alreadyVoted]);
 
     const handleVote = (
-        { combatId, boxerName }:
-        { combatId: number, boxerName: string }
+        { combatId, boxerId }:
+        { combatId: number, boxerId: string }
     ) => {
         const combatVote = votes[combatId]
         
-        if (combatVote.includes(boxerName)) {
-            const newVote = combatVote.filter((vote) => vote != boxerName )
-            setVotes(prevVote => prevVote.with(combatId, newVote))
+        if (combatVote == boxerId) {
+            setVotes(prevVote => prevVote.with(combatId, ""))
             return
-        }
+        }  
 
-        if (combatVote.length >= 1) return
-
-        const newVote = [...combatVote, boxerName]
-        setVotes(prevVotes => prevVotes.with(combatId, newVote))
+        setVotes(prevVotes => prevVotes.with(combatId, boxerId))
     }
 
     const sendVotes = (
         votes: Votes 
     ) => {
+        //TODO Save votes in DB - UserName + BoxerId ordered by combatId (index)
         const votesJSON = JSON.stringify(votes);
-        
         localStorage.setItem('votes', votesJSON);
+
+        setAlreadyVoted(true)
     }
 
-    /* TODO - Check the Votes in the storage
-    const checkStgVotes = () => {
-        const stgVotesJSON = localStorage.getItem('votes') || "";
+    if (alreadyVoted && storageVotes != undefined) {
+        return (
+            //TODO - Extract this to a component
+            <section class="animate-fade-in animate-duration-500 m-5 text-center text-primary">
+                <p class="uppercase text-3xl">Ya has realizado tus votos</p>
+                <div class="flex justify-center items-center mb-5">
+                    <div class="grid grid-cols-3 gap-4">
+                        {
+                            storageVotes.map((votedBoxer, i) => {
+                                const boxerIndex = parseInt(votedBoxer)-1
+                                return (
+                                    <div class="text-center">
+                                    <img src={combatInfo[i].boxers[boxerIndex].image} alt={combatInfo[i].boxers[boxerIndex].boxerName} class="w-24 h-24 rounded-full mx-auto mb-2"/>
+                                    <p class="text-sm font-medium">{combatInfo[i].boxers[boxerIndex].boxerName}</p>
+                                </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                <button class="transition hover:scale-125 font-medium font-atomic text-6xl uppercase" 
+                    onClick={() => {
+                        localStorage.removeItem("votes")
+                        //TODO - Remove values from DB
 
-        const stgVotes = JSON.parse(stgVotesJSON);
-
-        console.log(stgVotes)
-
-        return stgVotes
+                        setAlreadyVoted(false)
+                    }}>
+                        Volver a votar
+                </button>
+            </section>
+        )
     }
-    console.log(checkStgVotes())*/
   
     return (
         <section class="animate-fade-in animate-delay-[1s]">
@@ -85,14 +102,14 @@ export function VoteSystem () {
                             <ul class="flex flex-auto border-primary border-b-2">
                                 {
                                     combat?.boxers?.map((boxer, boxerI) => {
-                                        const combatVote = votes[combat.id - 1]
-                                        const isVoted = combatVote?.includes(boxer.boxerName)
+                                        const combatVote = parseInt(votes[combat.id - 1])
+                                        const isVoted = combatVote === parseInt(boxer.id)
                                         return (
                                             <li class={`group ${isVoted ? 'bg-gradient-to-t from-lime-400' : ''} w-full transition text-center sm:w-1/2 xl:w-1/2 overflow-hidden relative text-3xl md:w-1/2`}>
-                                                <button class="w-full h-full" onClick={() => handleVote({ combatId: combat.id - 1, boxerName: boxer.boxerName })}>
+                                                <button class="w-full h-full" onClick={() => handleVote({ combatId: combat.id - 1, boxerId: boxer.id })}>
                                                         <img 
                                                             src={combat.boxers[boxerI].image} 
-                                                            class={`${isVoted ? 'opacity-100 scale-110' : 'opacity-70'} ${!isVoted && combatVote.length > 0 ? 'opacity-20' : 'hover:scale-110 hover:opacity-100'} transition-all ease-in-out duration-500 rounded w-full h-full pt-8`}
+                                                            class={`${isVoted ? 'opacity-100 scale-110' : 'opacity-70'} ${!isVoted && combatVote >= 0 ? 'opacity-20' : 'hover:scale-110 hover:opacity-100'} transition-all ease-in-out duration-500 rounded w-full h-full pt-8`}
                                                         />
                                                 </button>
                                                 <span class="absolute bg-primary md:mx-40 m-4 text-base font-bold uppercase text-secondary bottom-7 left-0 right-0 z-20 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out pointer-events-none" aria-hidden="true">
